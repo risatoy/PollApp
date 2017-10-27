b2bbe990337a1e22b61c744b390fce1e3bd7c4d9
Risa Toyoshima <Risa.Toyoshima31@myhunter.cuny.edu>
2017-10-27 12:40:57 -0400

    First commit

---
+++ models/choices.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Choices = sequelize.define('Choices', {
+    description: DataTypes.STRING,
+    count: DataTypes.INTEGER
+  });
+
+  Choices.associate = (models) => {
+    // models.Choices.hasMany(models.Votes);
+    models.Choices.belongsTo(models.Polls);
+  }
+
+  return Choices;
+};
